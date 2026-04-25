85d3611db8a06ad2c9904d799ad390973fe7f3a6
piyapat2552p-rgb <piyapat2552p@gmail.com>
2026-04-25 06:48:18 +0000

    Complete exercise 00: create_array

---
+++ cell05/ex00/create_array.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    # สร้าง array (list) ของตัวเลขตามตัวอย่างในโจทย์
+    numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+    
+    # แสดงผล array ออกมาทางหน้าจอ
+    print(numbers)
+
+if __name__ == "__main__":
+    main()
