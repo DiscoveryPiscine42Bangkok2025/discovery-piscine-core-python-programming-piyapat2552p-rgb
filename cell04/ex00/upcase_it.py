19960483896452c52049395909bcd089d20bdc3f
piyapat2552p-rgb <piyapat2552p@gmail.com>
2026-04-25 03:48:26 +0000

    Add upcase_it exercise

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    # รับคำจากผู้ใช้
+    word = input("Give me a word: ")
+    
+    # ใช้ method .upper() เพื่อเปลี่ยนเป็นตัวพิมพ์ใหญ่
+    print(word.upper())
+
+if __name__ == "__main__":
+    main()
