9b932948deb76493022acc6490c4d6b0e9e43364
piyapat2552p-rgb <piyapat2552p@gmail.com>
2026-04-25 07:54:01 +0000

    Complete exercise 02: play_with_arrays++

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    
+    # สร้าง array ใหม่โดยมีเงื่อนไข:
+    # - เลือกเฉพาะตัวเลขที่ > 5 เท่านั้น
+    # - นำตัวเลขเหล่านั้นมาบวกเพิ่มอีก 2
+    new_array = [x + 2 for x in original_array if x > 5]
+    
+    print(original_array)
+    print(new_array)
+
+if __name__ == "__main__":
+    main()
