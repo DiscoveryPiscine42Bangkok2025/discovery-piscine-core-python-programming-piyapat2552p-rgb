ba404fe87aa3c178c899a435142e010ea779dd4d
piyapat2552p-rgb <piyapat2552p@gmail.com>
2026-04-26 04:09:25 +0000

    Finish Exercise 01: upcase_it with return method

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def upcase_it(s):
+    # คืนค่า (return) เป็นตัวพิมพ์ใหญ่
+    return s.upper()
+
+# ส่วนทดสอบ Method (Test the method)
+if __name__ == "__main__":
+   
+    print(upcase_it("hello"))
