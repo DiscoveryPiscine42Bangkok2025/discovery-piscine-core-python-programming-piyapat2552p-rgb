a1890e2ff3ac4c4d3209597e16f687169c295f14
piyapat2552p-rgb <piyapat2552p@gmail.com>
2026-04-26 04:05:42 +0000

    Add Exercise 01: Discovering Methods

---
+++ cell06/ex00/hello_all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Method (Method Definition)
+def hello():
+    print("Hello, everyone!")
+
+# การเรียกใช้ Method (Calling the method)
+if __name__ == "__main__":
+    hello()
